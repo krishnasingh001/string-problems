041497acc4e7c67a8d8a7b6eeb00c6c2a64e0772
Krishna Singh <krishnasinghcs@gmail.com>
2024-09-02 22:14:51 +0530

    Add the reverse vowels program

---
+++ reverse_vowels.rb
@@ -0,0 +1,18 @@
+class ReverseVowels
+  def reverse_vowels(input)
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    vowel_indices = []
+    input.chars.each_with_index do |char, index|
+      vowel_indices << index if vowels.include?(char)
+    end
+    i, j = 0, vowel_indices.size - 1
+
+    while i < j
+        input[vowel_indices[i]], input[vowel_indices[j]] = input[vowel_indices[j]], input[vowel_indices[i]]
+        i += 1
+        j -= 1
+    end
+    input
+  end
+end
